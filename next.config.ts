6f3f5c3cff90b24f16d8f05f412753a2e12430f9
Marco Martínez <marco.martinez.s@ug.uchile.cl>
2026-01-05 11:57:54 -0300

    add: cors allow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "19f86181fa27.ngrok-free.app",
+    "https://19f86181fa27.ngrok-free.app"
+  ],
 };
 
 export default nextConfig;
