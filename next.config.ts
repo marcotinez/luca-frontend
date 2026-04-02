eddb5330ca6b8a57374ab011b0087f6971f2859d
Marco Martínez <marco.martinez.s@ug.uchile.cl>
2026-04-02 09:46:32 -0300

    chore(dev): actualizar origenes y artefactos locales

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
     "19f86181fa27.ngrok-free.app",
-    "https://19f86181fa27.ngrok-free.app"
+    "https://19f86181fa27.ngrok-free.app",
+    "pop-os.tail44b053.ts.net"
   ],
 };
 
